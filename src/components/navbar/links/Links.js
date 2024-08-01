7c8267ddb0567f7c374d1a4a711d767648d5e1a7
AfrideHossain <afredehossain@gmail.com>
2024-08-01 21:28:48 +0600

    first commit

---
+++ src/components/navbar/links/Links.js
@@ -0,0 +1,32 @@
+import Link from "next/link";
+import React from "react";
+
+export default function Links() {
+  const links = [
+    {
+      title: "Homepage",
+      path: "/",
+    },
+    {
+      title: "About",
+      path: "/about",
+    },
+    {
+      title: "Contact",
+      path: "/contact",
+    },
+    {
+      title: "Blog",
+      path: "/blog",
+    },
+  ];
+  return (
+    <div>
+      {links.map((link) => (
+        <Link key={link.title} href={link.path}>
+          {link.title}
+        </Link>
+      ))}
+    </div>
+  );
+}
